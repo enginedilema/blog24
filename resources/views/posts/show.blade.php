36bcb4603775fd39a244c14a5276d0680e59d48b
enginedilema <omiro@xtec.cat>
2024-10-10 18:01:46 +0200

    Update show.blade.php for a post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,7 @@
     <h1>{{$post->title}}</h1>
     <p>{{$post->content}}</p>
     <p>Created at: {{$post->created_at}}</p>
+    <p>Updated at: {{$post->updated_at}}</p>
     <p>Author : {{$post->user->name}}</p>
 </body>
-</html>
+</html> 
